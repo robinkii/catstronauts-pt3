ee011b20be1b620c0c92196f5d5ed8390af6c37d
Robin Kelsick II <robinkelsick@gmail.com>
2023-11-25 02:47:17 -0500

    Add modules to Track schema

--- server/src/schema.js
+++ server/src/schema.js
@@ -23,6 +23,8 @@ const typeDefs = gql`
     description: String
     "The number of times a track has been viewed"
     numberOfViews: Int
+    "The track's complete array of Modules"
+    modules: [Module!]!
   }
 
   "Author of a complete Track or a Module"
@@ -32,6 +34,15 @@ const typeDefs = gql`
     name: String!
     "Author's profile picture"
     photo: String
+  }
+
+  "Module is a single unit of teaching. Multiple Modules compose a Track"
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
   }
 `;
 
