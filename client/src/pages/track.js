e3080398966b88f632becd57215c0d6d1be8ed50
Robin Kelsick II <robinkelsick@gmail.com>
2023-11-25 13:33:50 -0500

    created and exported single track page

---
+++ client/src/pages/track.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { Layout, QueryResult } from '../components';
+import { useParams } from 'react-router-dom';
+
+const Track = () => {
+  const { trackId = '' } = useParams();
+
+  return <Layout></Layout>;
+};
+
+export default Track;
